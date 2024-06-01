5a32a265b848a1f16a994a6d5198d132af4b7899
Zishan Rahman <zishanfrahman@outlook.com>
2024-06-01 10:23:26 +0100

    Change number of observers added to subject in test1 in observer.py to 8000
    Added as parameter input to test1 instead of changing ObserverTest default addcount

--- observer.py
+++ observer.py
@@ -88,7 +88,7 @@ class ObserverTest:
         self.fill()
 
 def main() -> None:
-    test1 = ObserverTest()
+    test1 = ObserverTest(8000)
     print(f"Time taken to perform test with {test1.n_views} observers for 1 subject: {time(test1.test)}ms")
     test1.fill()
     print(f"Time taken to perform test with {test1.n_views} observers for 1 subject: {time(test1.test)}ms")
